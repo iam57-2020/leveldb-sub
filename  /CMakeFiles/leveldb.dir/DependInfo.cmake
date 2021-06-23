a48dec90f08908f5c3a6dd5fdebc6dfb27d114dc
wq <907721660@qq.com>
2021-06-23 11:13:29 +0800

    init

---
+++  /CMakeFiles/leveldb.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wu/leveldb_sub/leveldb/db/builder.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/builder.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/c.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/c.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/db_impl.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/db_impl.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/db_iter.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/db_iter.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/dbformat.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/dbformat.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/dumpfile.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/dumpfile.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/filename.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/filename.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/log_reader.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/log_reader.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/log_writer.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/log_writer.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/memtable.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/memtable.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/repair.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/repair.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/table_cache.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/table_cache.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/version_edit.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/version_edit.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/version_set.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/version_set.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/db/write_batch.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/db/write_batch.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/helpers/memenv/memenv.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/helpers/memenv/memenv.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/table/block.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/table/block.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/table/block_builder.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/table/block_builder.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/table/filter_block.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/table/filter_block.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/table/format.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/table/format.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/table/iterator.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/table/iterator.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/table/merger.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/table/merger.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/table/table.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/table/table.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/table/table_builder.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/table/table_builder.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/table/two_level_iterator.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/table/two_level_iterator.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/arena.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/arena.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/bloom.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/bloom.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/cache.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/cache.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/coding.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/coding.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/comparator.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/comparator.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/crc32c.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/crc32c.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/env.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/env.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/env_posix.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/env_posix.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/filter_policy.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/filter_policy.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/hash.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/hash.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/logging.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/logging.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/options.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/options.cc.o"
+  "/Users/wu/leveldb_sub/leveldb/util/status.cc" "/Users/wu/leveldb_sub/leveldb/build/CMakeFiles/leveldb.dir/util/status.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LEVELDB_COMPILE_LIBRARY"
+  "LEVELDB_PLATFORM_POSIX=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "../."
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++  /third_party/benchmark/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wu/leveldb_sub/leveldb/third_party/benchmark/src
+# Build directory: /Users/wu/leveldb_sub/leveldb/build/third_party/benchmark/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
